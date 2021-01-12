4660eacb018b64d4298e37fc7caf1d49ba3fedfc
josuetp163 <jotopozo@espol.edu.ec>
2021-01-11 20:23:46 -0500

    web app

---
+++ src/app/pages/estaciones/estaciones.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-estaciones',
+  templateUrl: './estaciones.component.html',
+  styleUrls: ['./estaciones.component.css']
+})
+export class EstacionesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
