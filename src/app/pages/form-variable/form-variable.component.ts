6ec5ee74fa5aad90b3fb4b26e6d516a1c550968a
Nicole Alvarez <ngalvare@hotmail.com>
2021-01-21 16:24:18 -0500

    [UDATE] el formulario de varibale para envio al backend

--- src/app/pages/form-variable/form-variable.component.ts
+++ src/app/pages/form-variable/form-variable.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { Variable } from '../../modelos/variable';
+import { NgForm} from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 
 @Component({
   selector: 'app-form-variable',
   templateUrl: './form-variable.component.html',
   styleUrls: ['./form-variable.component.css']
 })
+
+@Injectable({
+  providedIn: 'root'
+})
+
 export class FormVariableComponent implements OnInit {
 
-  constructor() { }
+  variable= new Variable();
+  
+  constructor( 
+    private http: HttpClient) { }
 
   ngOnInit(): void {
   }
+
+  onSubmit(formEstacion: NgForm){
+    this.http.post("http://localhost:3000/varibales/new",this.variable).subscribe(
+      data => {
+        console.log("enviado")
+        //this.showNotification();
+      },
+      err => {
+        console.log("Errorrr")
+        console.log(err)
+      }
+    )
+
+  }
+
+
 
 }
