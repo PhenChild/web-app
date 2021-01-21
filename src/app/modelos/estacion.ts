9c1f52b9f39ccd384d32ff7bc03d6afc2217bf0c
Nicole Alvarez <ngalvare@hotmail.com>
2021-01-20 21:24:53 -0500

    Se añadio el modelo de estacion

---
+++ src/app/modelos/estacion.ts
@@ -0,0 +1,13 @@
+import { NumberFormatStyle } from "@angular/common";
+
+export class Estacion {
+    id: number
+    codigoEstacion: string 
+    nombreEstacion: string
+    latitud: number
+    longitud: number
+    altitud: number
+    suelo: string
+    omm: string
+
+}
