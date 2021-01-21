349527f5e7a6a782ba0c3f5e167607da50d14ad7
josuetp163 <jotopozo@espol.edu.ec>
2021-01-20 21:30:45 -0500

    modelos

--- src/app/pages/usuarios/usuarios.component.ts
+++ src/app/pages/usuarios/usuarios.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-usuarios',
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuariosComponent implements OnInit {
 
+  dtOptions: Datatables.Settings = {};
+  usuarios: Usuario[] = [];
+
+  dtTrigger: Subject<any> = new Subject<any>()
   constructor() { }
 
   ngOnInit(): void {
