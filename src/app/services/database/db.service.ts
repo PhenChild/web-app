5a6973588562d82d42a5f3cfd7b62d4608a1858c
Josue Tomala <jotopozo@espol.edu.ec>
2021-06-29 14:11:32 -0500

    [ADD] dburl

--- src/app/services/database/db.service.ts
+++ src/app/services/database/db.service.ts
@@ -16,8 +16,8 @@ export class DbService {
     usuarios: Usuario[] = [];
 
     /** Url de conexión */
-    //  dbURL = "https://phenapp.loca.lt/api/";
-    dbURL = "https://" + environment.host + ":" + environment.apiport + "/api/";
+    dbURL = "https://phenapp5.loca.lt/api/";
+    // dbURL = "https://" + environment.host + ":" + environment.apiport + "/api/";
 
     /**
      * Constructor
